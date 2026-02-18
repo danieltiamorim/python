a3f035fa659e1b6ec54a4322527fea7a78818e33
danieltiamorim <danieltiamorim@gmail.com>
2026-02-17 22:40:36 -0300

    Finishing chapter 2 (Files.ipynb) + swimclub.py fi les.

--- head-first/swimclub.py
+++ head-first/swimclub.py
@@ -2,16 +2,25 @@ import statistics
 FOLDER = ("swimdata/")
 
 def read_swim_data(filename):
+    """Return swim data from a file.
+    
+    Given the name of a swimmer's file (in filename), extract all the required
+    data, then return it to the caller as tuple.
+    """
     swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
     with open(FOLDER+filename) as file:
         lines = file.readlines()
         times = lines[0].strip().split(",")
         converts = []
         for t in times:
-            minutes, rest = t.split(":")
-            seconds,hundredths = rest.split(".")
-            converts.append((int(minutes) * 60 * 100) + (int(seconds) * 100) + int(hundredths))
-            
+            # The Minutes value might be missing, so guard against this causing a crash.
+            if ":" in t:
+                minutes, rest = t.split(":")
+                seconds, hundredths = rest.split(".")
+            else:
+                minutes = 0
+                seconds, hundredths = t.split(".")
+            converts.append((int(minutes) * 60 * 100) + (int(seconds) * 100) + int(hundredths))     
     average = statistics.mean(converts)
     min_secs, hundredths = str(round(average / 100, 2 )).split(".")
     min_secs = int(min_secs)
@@ -19,4 +28,4 @@ def read_swim_data(filename):
     seconds = min_secs - minutes * 60
     average = str(minutes) + ":" + str(seconds) + "." + str(hundredths)
     
-    return swimmer, age, distance, stroke, times, average
+    return swimmer, age, distance, stroke, times, average  # Returned as tuple.
